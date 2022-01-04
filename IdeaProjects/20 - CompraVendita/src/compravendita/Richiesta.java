2d40f53d601ce56021b34d2e7dabf3d12496c929
Leonardo Tocchet <leonardotocchet@gmail.com>
2022-01-04 21:08:22 +0100

    Added Richiesta class

---
+++ IdeaProjects/20 - CompraVendita/src/compravendita/Richiesta.java
@@ -0,0 +1,47 @@
+package compravendita;
+
+import java.util.Objects;
+
+public class Richiesta {
+    private int iId;
+    private int iQuantita;
+
+    public Richiesta(int iId) {
+        this.iId = iId;
+    }
+
+    public int getQuantita() {
+        return iQuantita;
+    }
+
+    public void setQuantita(int iQuantita) {
+        this.iQuantita = iQuantita;
+    }
+
+    public int getId() {
+        return iId;
+    }
+
+    public void setId(int iId) {
+        this.iId = iId;
+    }
+
+    @Override
+    public String toString() {
+        return "Richiesta{" +
+                "iId=" + getId() +
+                ", iQuantita=" + getQuantita() +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object oObject) {
+        if(!(oObject instanceof Richiesta oRichiesta))
+            return false;
+
+        if(this == oObject)
+            return true;
+
+        return iId == oRichiesta.getId() && iQuantita == oRichiesta.getQuantita() ;
+    }
+}
